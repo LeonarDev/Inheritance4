7f24e3111b71a73df45170ef2b9b821f673dc2a6
LeonarDev <leonardo.majevski@gmail.com>
2021-09-01 15:11:34 -0300

    init

---
+++ Inheritance4/Entities/Circle.cs
@@ -0,0 +1,20 @@
+﻿using Inheritance4.Entities.Enums;
+using System;
+
+namespace Inheritance4.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
